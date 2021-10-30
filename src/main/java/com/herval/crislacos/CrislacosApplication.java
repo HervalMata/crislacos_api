90f5438413271b8500ef59e780e475447f5fa821
Herval Mata <hervalmataamparo@gmail.com>
2021-10-30 16:10:16 -0300

    feat: art: api - setup project

---
+++ src/main/java/com/herval/crislacos/CrislacosApplication.java
@@ -0,0 +1,13 @@
+package com.herval.crislacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrislacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrislacosApplication.class, args);
+    }
+
+}
